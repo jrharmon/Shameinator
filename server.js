3b9aebb4f8c892a5753bcec71dadf5766fd2ccdd
jrharmon <jrharmon@gmail.com>
2015-10-07 08:06:35 -0400

    copied over initial local code

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket) {
+    socket.on('shame', function(msg) {
+       console.log('shamed: ' + msg);
+       socket.broadcast.emit('shame', msg); 
+    });
+})
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
